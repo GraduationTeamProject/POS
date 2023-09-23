b1ab82155321132f80e9eb5b25e72beee0055430
mxxjee <kmj694288@gmail.com>
2023-09-23 22:14:24 +0900

    날씨에 따른 빛설정 추가예정

---
+++ POS/Assets/WeatherManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static bool isRain;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(isRain)
+    }
+
+    //IEnumerator Rain()
+    //{
+
+    //}
+}
